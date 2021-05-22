ce4927a137186318bb822726f6ed20c5cc35d3d8
ATZIN NATANAEL PACHECO ARELLANO <Atzin.Pacheco@alumno.udg.mx>
2021-05-22 01:26:44 -0500

    Agregar archivos de proyecto.

---
+++ CARRERA CABALLOS/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace CARRERA_CABALLOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.caballo1 = new System.Windows.Forms.PictureBox();
+            this.caballo2 = new System.Windows.Forms.PictureBox();
+            this.caballo3 = new System.Windows.Forms.PictureBox();
+            this.caballo4 = new System.Windows.Forms.PictureBox();
+            this.caballo5 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Caballo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Posición = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Asap", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(1210, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "REINICIAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Asap", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(1210, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "INICIAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(22, 78);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1182, 409);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Asap", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(512, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "CARRERA DE CABALLOS";
+            // 
+            // caballo1
+            // 
+            this.caballo1.BackColor = System.Drawing.Color.Transparent;
+            this.caballo1.Image = ((System.Drawing.Image)(resources.GetObject("caballo1.Image")));
+            this.caballo1.Location = new System.Drawing.Point(22, 167);
+            this.caballo1.Name = "caballo1";
+            this.caballo1.Size = new System.Drawing.Size(150, 65);
+            this.caballo1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.caballo1.TabIndex = 9;
+            this.caballo1.TabStop = false;
+            // 
+            // caballo2
+            // 
+            this.caballo2.BackColor = System.Drawing.Color.Transparent;
+            this.caballo2.Image = ((System.Drawing.Image)(resources.GetObject("caballo2.Image")));
+            this.caballo2.Location = new System.Drawing.Point(22, 249);
+            this.caballo2.Name = "caballo2";
+            this.caballo2.Size = new System.Drawing.Size(150, 65);
+            this.caballo2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.caballo2.TabIndex = 11;
+            this.caballo2.TabStop = false;
+            // 
+            // caballo3
+            // 
+            this.caballo3.BackColor = System.Drawing.Color.Transparent;
+            this.caballo3.Image = ((System.Drawing.Image)(resources.GetObject("caballo3.Image")));
+            this.caballo3.Location = new System.Drawing.Point(22, 331);
+            this.caballo3.Name = "caballo3";
+            this.caballo3.Size = new System.Drawing.Size(150, 65);
+            this.caballo3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.caballo3.TabIndex = 12;
+            this.caballo3.TabStop = false;
+            // 
+            // caballo4
+            // 
+            this.caballo4.BackColor = System.Drawing.Color.Transparent;
+            this.caballo4.Image = ((System.Drawing.Image)(resources.GetObject("caballo4.Image")));
+            this.caballo4.Location = new System.Drawing.Point(22, 411);
+            this.caballo4.Name = "caballo4";
+            this.caballo4.Size = new System.Drawing.Size(150, 65);
+            this.caballo4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.caballo4.TabIndex = 13;
+            this.caballo4.TabStop = false;
+            // 
+            // caballo5
+            // 
+            this.caballo5.BackColor = System.Drawing.Color.Transparent;
+            this.caballo5.Image = ((System.Drawing.Image)(resources.GetObject("caballo5.Image")));
+            this.caballo5.Location = new System.Drawing.Point(22, 456);
+            this.caballo5.Name = "caballo5";
+            this.caballo5.Size = new System.Drawing.Size(150, 65);
+            this.caballo5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.caballo5.TabIndex = 14;
+            this.caballo5.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Asap", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 200);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 22);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Asap", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 250);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(20, 22);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Asap", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(3, 300);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 22);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Asap", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, 350);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 22);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "4";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Asap", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(3, 400);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 22);
+            this.label6.TabIndex = 19;
+            this.label6.Text = "5";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.OliveDrab;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Caballo,
+            this.Posición});
+            this.dataGridView1.Location = new System.Drawing.Point(1210, 233);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(211, 208);
+            this.dataGridView1.TabIndex = 20;
+            // 
+            // Caballo
+            // 
+            this.Caballo.HeaderText = "Caballo";
+            this.Caballo.Name = "Caballo";
+            // 
+            // Posición
+            // 
+            this.Posición.HeaderText = "Posición";
+            this.Posición.Name = "Posición";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.OliveDrab;
+            this.ClientSize = new System.Drawing.Size(1433, 535);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.caballo2);
+            this.Controls.Add(this.caballo1);
+            this.Controls.Add(this.caballo5);
+            this.Controls.Add(this.caballo4);
+            this.Controls.Add(this.caballo3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "E";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.caballo5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        public System.Windows.Forms.PictureBox caballo1;
+        public System.Windows.Forms.PictureBox caballo2;
+        public System.Windows.Forms.PictureBox caballo3;
+        public System.Windows.Forms.PictureBox caballo4;
+        public System.Windows.Forms.PictureBox caballo5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Caballo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Posición;
+    }
+}
+
---
+++ CARRERA CABALLOS/Form1.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CARRERA_CABALLOS
+{
+    public partial class Form1 : Form
+    {
+        Random random = new Random();
+        Thread hilo;
+        Thread hilo2;
+        Thread hilo3;
+        Thread hilo4;
+        Thread hilo5;
+        int x;
+        int o;
+        int v;
+        int z;
+        int u;
+        int velocidadchida;
+        int velocidadchida2;
+        int velocidadchida3;
+        int velocidadchida4;
+        int velocidadchida5;
+        int posicion=1;
+        bool vivo = true;
+        bool vivo2 = true;
+        bool vivo3 = true;
+        bool vivo4 = true;
+        bool vivo5 = true;
+        public Form1()
+        {
+            InitializeComponent();
+            pictureBox1.Controls.Add(caballo1);
+            pictureBox1.Controls.Add(caballo2);
+            pictureBox1.Controls.Add(caballo3);
+            pictureBox1.Controls.Add(caballo4);
+            pictureBox1.Controls.Add(caballo5);
+            caballo1.Location = new Point(20, 100);
+            caballo2.Location = new Point(20, 150);
+            caballo3.Location = new Point(20, 200);
+            caballo4.Location = new Point(20, 250);
+            caballo5.Location = new Point(20, 300);
+            caballo1.BackColor = Color.Transparent;
+            caballo2.BackColor = Color.Transparent;
+            caballo3.BackColor = Color.Transparent;
+            caballo4.BackColor = Color.Transparent;
+            caballo5.BackColor = Color.Transparent;
+            hilo = new Thread(caballo11);
+            CheckForIllegalCrossThreadCalls = false;
+            hilo2 = new Thread(caballo22);
+            CheckForIllegalCrossThreadCalls = false;
+            hilo3 = new Thread(caballo33);
+            CheckForIllegalCrossThreadCalls = false;
+            hilo4 = new Thread(caballo44);
+            CheckForIllegalCrossThreadCalls = false;
+            hilo5 = new Thread(caballo55);
+            CheckForIllegalCrossThreadCalls = false;
+
+        }
+        void caballo11()
+        {
+            
+            int y = caballo1.Top;
+            x = caballo1.Left;
+            int limite = 1180;
+            for (x = caballo1.Left; x < limite; x++)
+            {
+                velocidadchida = random.Next(0, 2);
+                caballo1.Location = new Point(x, y);
+                caballo1.Refresh();
+                Thread.Sleep(velocidadchida);
+                if (x == limite-120)
+                {
+                    dataGridView1.Rows.Add();
+                    int cantidad = dataGridView1.Rows.Count - 1;
+                    dataGridView1[0, cantidad].Value = "Caballo 1";
+                    dataGridView1[1, cantidad].Value = posicion;
+                    posicion++;
+                    vivo = false;
+                }
+                if (x == limite - 1)
+                {
+                    hilo.Suspend();
+                }
+            }
+        }
+        void caballo22()
+        {
+            int y = caballo2.Top;
+            z = caballo2.Left;
+            int limite = 1180;
+            for (z= caballo2.Left; z < limite; z++)
+            {
+                velocidadchida2 = random.Next(0, 2);
+                caballo2.Location = new Point(z, y);
+                caballo2.Refresh();
+                Thread.Sleep(velocidadchida2);
+                if (z == limite-120)
+                {
+                    dataGridView1.Rows.Add();
+                    int cantidad = dataGridView1.Rows.Count - 1;
+                    dataGridView1[0, cantidad].Value = "Caballo 2";
+                    dataGridView1[1, cantidad].Value = posicion;
+                    posicion++;
+                    vivo2 = false;
+                }
+                if (z == limite - 1)
+                {
+                    hilo2.Suspend();
+                }
+            }
+
+        }
+        void caballo33()
+        {
+            int y = caballo3.Top;
+            u = caballo3.Left;
+            int limite = 1180;
+            for (u = caballo3.Left; u < limite; u++)
+            {
+            velocidadchida3 = random.Next(0, 2);
+                caballo3.Location = new Point(u, y);
+                caballo3.Refresh();
+                Thread.Sleep(velocidadchida3);
+                if (u == limite-120)
+                {
+                    dataGridView1.Rows.Add();
+                    int cantidad = dataGridView1.Rows.Count - 1;
+                    dataGridView1[0, cantidad].Value = "Caballo 3";
+                    dataGridView1[1, cantidad].Value = posicion;
+                    posicion++;
+                    vivo3 = false;
+                }
+                if (u == limite - 1)
+                {
+                    hilo3.Suspend();
+                }
+            }
+
+        }
+        void caballo44()
+        {
+            int y = caballo4.Top;
+            v = caballo4.Left;
+            int limite = 1180;
+            for (v = caballo4.Left; v < limite; v++)
+            {
+            velocidadchida4 = random.Next(0, 2);
+                caballo4.Location = new Point(v, y);
+                caballo4.Refresh();
+                Thread.Sleep(velocidadchida4);
+                if (v == limite-120)
+                {
+                    dataGridView1.Rows.Add();
+                    int cantidad = dataGridView1.Rows.Count - 1;
+                    dataGridView1[0, cantidad].Value = "Caballo 4";
+                    dataGridView1[1, cantidad].Value = posicion;
+                    posicion++;
+                    vivo4 = false;
+                }
+                if (v == limite - 1)
+                {
+                    hilo4.Suspend();
+                }
+            }
+
+        }
+        void caballo55()
+        {
+            int y = caballo5.Top;
+            o = caballo5.Left;
+            int limite = 1180;
+            for (o = caballo5.Left; o < limite; o++)
+            {
+            velocidadchida5 = random.Next(0, 2);
+                caballo5.Location = new Point(o, y);
+                caballo5.Refresh();
+                Thread.Sleep(velocidadchida5);
+                if (o == limite-120)
+                {
+                    dataGridView1.Rows.Add();
+                    int cantidad = dataGridView1.Rows.Count - 1;
+                    dataGridView1[0, cantidad].Value = "Caballo 5";
+                    dataGridView1[1, cantidad].Value = posicion;
+                    posicion++;
+                    vivo5 = false;
+                }
+                if (o == limite - 1)
+                {
+                    hilo5.Suspend();
+                }
+            }
+
+        }
+        /*
+        void pista(PictureBox caballo, int velocidad)
+        {
+            int y = caballo.Top;
+            int limite = 1180;
+            for (; caballo.Left < limite; caballo.Left++)
+            {
+                caballo.Location = new Point(caballo.Left, y);
+                caballo.Refresh();
+                Thread.Sleep(velocidad);
+            }
+
+        }*/
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hilo.Start();
+            hilo2.Start();
+            hilo3.Start();
+            hilo4.Start();
+            hilo5.Start();
+            button2.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            if(vivo == false)
+            {
+                caballo1.Top = 100;
+                caballo1.Left = 20;
+                x = caballo1.Left;
+                velocidadchida = random.Next(0, 2);
+                hilo.Resume();
+                vivo = true;
+            }
+            if (vivo2 == false)
+            {
+                caballo2.Top = 150;
+                caballo2.Left = 20;
+                z = caballo2.Left;
+                velocidadchida2 = random.Next(0, 2);              
+                hilo2.Resume();
+                vivo2 = true;
+            }
+            if (vivo3 == false)
+            {
+                caballo3.Top = 200;
+                caballo3.Left = 20;
+                u = caballo3.Left;
+                velocidadchida3 = random.Next(0, 2);
+                hilo3.Resume();
+                vivo3 = true;
+            }
+            if (vivo4 == false)
+            {
+                caballo4.Top = 250;
+                caballo4.Left = 20;
+                v = caballo4.Left;
+                velocidadchida4 = random.Next(0, 2);
+                hilo4.Resume();
+                vivo4 = true;
+            }
+            if (vivo5 == false)
+            {
+                caballo5.Top = 300;
+                caballo5.Left = 20;
+                o = caballo5.Left;
+                velocidadchida5 = random.Next(0, 2);
+                hilo5.Resume();
+                vivo5 = true;
+            }
+            hilo.Suspend();
+            caballo1.Top = 100;
+            caballo1.Left = 20;
+            x = caballo1.Left;
+            velocidadchida = random.Next(0, 2);
+            hilo2.Suspend();
+            caballo2.Top = 150;
+            caballo2.Left = 20;
+            z = caballo2.Left;
+            velocidadchida2 = random.Next(0, 2);
+            hilo3.Suspend();
+            caballo3.Top = 200;
+            caballo3.Left = 20;
+            u = caballo3.Left;
+            velocidadchida3 = random.Next(0, 2);
+            hilo4.Suspend();
+            caballo4.Top = 250;
+            caballo4.Left = 20;
+            v = caballo4.Left;
+            velocidadchida4 = random.Next(0, 2);
+            hilo5.Suspend();
+            caballo5.Top = 300;
+            caballo5.Left = 20;
+            o = caballo5.Left;
+            velocidadchida5 = random.Next(0, 2);
+            pictureBox1.Refresh();
+            caballo1.Refresh();
+            caballo2.Refresh();
+            caballo3.Refresh();
+            caballo4.Refresh();
+            caballo5.Refresh();
+            posicion = 1;
+            hilo.Resume();
+            hilo2.Resume();
+            hilo3.Resume();
+            hilo4.Resume();
+            hilo5.Resume();
+        }
+    }
+}
